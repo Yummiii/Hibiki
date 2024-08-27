b013a76c727aa79fcbc677ea00fb500a17b59963
Yummiii <Leeo05050@protonmail.com>
2024-08-27 19:13:52 -0300

    seek video

--- src/ui/footer/time_bar.rs
+++ src/ui/footer/time_bar.rs
@@ -1,13 +1,16 @@
 use crate::{messenger::MessageType, streamer::ArcPipe};
-use gstreamer::{prelude::ElementExtManual, ClockTime};
+use gstreamer::{
+    prelude::{ElementExt, ElementExtManual},
+    ClockTime, SeekFlags, State,
+};
 use gtk4::{
-    glib::{property::PropertySet, timeout_add_local, ControlFlow},
-    prelude::{RangeExt, WidgetExt},
+    glib::{property::PropertySet, timeout_add_local, ControlFlow, Propagation},
+    prelude::{ObjectExt, RangeExt, ScaleExt, WidgetExt},
     Scale,
 };
 use std::{
     sync::{
-        atomic::{AtomicU64, Ordering},
+        atomic::{AtomicBool, AtomicU64, Ordering},
         Arc,
     },
     time::Duration,
@@ -28,18 +31,49 @@ pub fn build_time_bar(pipeline: ArcPipe) -> Scale {
             });
     }
 
+    time_bar.connect_change_value({
+        let pipeline = pipeline.clone();
+        let duration = duration.clone();
+        let time_bar = time_bar.clone();
+
+        move |_, _, _value| {
+            // pipeline.playbin.set_state(State::Paused).unwrap();
+
+            let duration = duration.load(Ordering::Relaxed);
+            let width = time_bar.width() as f64;
+            let pos = (time_bar.value() / width) * duration as f64;
+            let pos = ClockTime::from_mseconds(pos as u64);
+
+            println!("Pos: {}", pos);
+            println!("Duartion: {}", ClockTime::from_mseconds(duration));
+
+            //this sometimes freezes the video, but i have no idea why
+            if let Ok(()) = pipeline
+                .playbin
+                .seek_simple(SeekFlags::TRICKMODE | SeekFlags::FLUSH, pos)
+            {
+                pipeline.playbin.set_state(State::Playing).unwrap();
+            }
+
+            Propagation::Proceed
+        }
+    });
+
     timeout_add_local(Duration::from_millis(1), {
         let pipeline = pipeline.clone();
         let time_bar = time_bar.clone();
 
         move || {
             let duration = duration.load(Ordering::Relaxed);
-            if duration > 0 {
+            if duration > 0 && pipeline.playbin.current_state() == State::Playing {
+                // println!("{:?}", pipeline.playbin.current_state());
+
                 let width = time_bar.width() as f64;
                 time_bar.set_range(0., width);
 
                 if let Some(pos) = pipeline.playbin.query_position::<ClockTime>() {
                     let scale = (width / duration as f64) * pos.mseconds() as f64;
+                    // println!("{}", scale);
                     time_bar.set_value(scale);
                 }
             }
